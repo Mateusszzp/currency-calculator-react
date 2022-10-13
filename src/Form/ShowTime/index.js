cc276d8adf718cc994d239ed13a5c139a3fb1bc7
Mateusszzp <mateusz.pasko.r@gmail.com>
2022-10-13 12:38:55 +0200

    added time

--- src/Form/ShowTime/index.js
+++ src/Form/ShowTime/index.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
-
+import "./style.css";
 const ShowTime = () => {
-   const [time, setTime] = useState("")
+    const [time, setTime] = useState("")
 
 
-   useEffect(() => {
-    setInterval(()=> {
-   setTime(new Date().toLocalString())
-    
-    },1000)
-})
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleString(undefined, {
+                weekday: "long", day: "numeric", month: "long", hour: "numeric",
+                minute: "numeric", second: "numeric"
+            }))
+        }, 1000)
+    })
 
 
-   
+
 
 
     return (
         <p>
-            Zegar {time}
-           
+           <span className="time">{time}</span> 
         </p>
     )
 }
