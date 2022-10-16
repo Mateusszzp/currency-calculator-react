2de54c54436cfe2698bc679c4ff7c3b806df45ef
Mateusszzp <mateusz.pasko.r@gmail.com>
2022-10-16 08:40:34 +0200

    time view

--- src/Form/ShowTime/index.js
+++ src/Form/ShowTime/index.js
@@ -3,28 +3,36 @@ import React, { useState, useEffect, } from 'react';
 
 
 
-    const ShowTime = () => {
-        const [time, setTime] = useState("")
-    
-    
-        useEffect(() => {
-            setInterval(() => {
-                setTime(new Date().toLocaleString(undefined, {
-                    weekday:"long",
-                    day: "numeric", 
-                    month: "long", 
-                    hour: "numeric",
-                    minute: "numeric", 
-                    second: "numeric",
-                }))
-            }, 1000)
-        })
+const ShowTime = () => {
+  const [time, setTime] = useState(new Date().toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }))
 
-    return (
-        <p>
-            <span className="time">Dzisiaj jest {time}</span>
-        </p>
-    )
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      }))
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [time]);
+  return (
+    <p>
+      <span className="time">Dzisiaj jest {time}</span>
+    </p>
+  )
 }
 export default ShowTime
 
