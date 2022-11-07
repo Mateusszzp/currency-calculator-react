6f5f9e3047a9aa09f9e0bf8c2c2bf8a122537889
Mateusszzp <mateusz.pasko.r@gmail.com>
2022-11-07 11:53:00 +0100

    adding custom hook useCurrentDate

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,31 +1,12 @@
-import React, { useState, useEffect, } from 'react';
 import { Time } from "./styled";
+import { useCurrentDate } from "./useCurrentDate"
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date());
-
-  const myDate = (time) => {
-    return time.toLocaleString(undefined, {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-    })
-  };
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => {
-      clearInterval(intervalId);
-    }
-  }, [time]);
+  const { time, myDate } = useCurrentDate()
 
   return (
     <p>
-      <Time className="time">
+      <Time>
         Dzisiaj jest {myDate(time)}
       </Time>
     </p>
---
+++ src/Form/Clock/useCurrentDate.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+export const useCurrentDate = () => {
+  const [time, setTime] = useState(new Date());
+
+  const myDate = (time) => {
+    return time.toLocaleString(undefined, {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    })
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [time]);
+    return { myDate, time }
+};
