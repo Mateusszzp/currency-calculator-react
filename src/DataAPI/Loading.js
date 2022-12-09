d6394cbd838561a6bacd7d0d0665998b47cc4a53
Mateusszzp <mateusz.pasko.r@gmail.com>
2022-12-09 18:38:59 +0100

    change in gitigno and add Loading and API

---
+++ src/DataAPI/Loading.js
@@ -0,0 +1,9 @@
+import { Bars } from 'react-loading-icons'
+
+const Loading = (
+    < Bars stroke="green"
+        strokeOpacity={.75}
+        strokeWidth={"center"}
+    />
+)
+export default Loading
